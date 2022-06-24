f5498f657fd96cd67f31d8342c63f585951dabc0
Ustun Ertas <ustunertas0@gmail.com>
2022-06-24 19:38:14 +0200

    day1

---
+++ src/test/java/Query01.java
@@ -0,0 +1,41 @@
+import java.sql.*;
+
+public class Query01 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        //1) Driver Yükle
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //2) Bağlanti Oluştur
+        Connection con =  DriverManager.getConnection("jdbc:mysql://localhost:3305/okul?serverTimezone=UTC", "root", "1234");
+        // okul: bağlanti yapacağımız database'in ismi
+
+        //3) Statement: SQL kodlarimizi yazmak için bir nesne oluştur
+        Statement st = con.createStatement();
+
+        //4) ResultSet:
+        ResultSet veri = st.executeQuery("select * from ogrenciler");
+
+        //5) sonuçları al
+
+        while (veri.next()){
+           // System.out.println(veri.getInt(1) + veri.getString(2)
+            //        + veri.getString(3) + veri.getString(4));
+
+            System.out.printf("%-6d %-15.15s %-8s %-8s\n", veri.getInt(1),
+                    veri.getString(2), veri.getString(3), veri.getString(4));
+
+
+        }
+
+        //kapatma
+        con.close();
+        st.close();
+        veri.close();
+
+
+
+
+    }
+}
---
+++ src/test/java/Query02.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+
+public class Query02 {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection con =  DriverManager.getConnection("jdbc:mysql://localhost:3305/okul?serverTimezone=UTC", "root", "1234");
+
+        Statement st = con.createStatement();
+
+        // SORU01: ÖĞRENCİLER TABLOSUNDAKİ ERKEK ÖĞRENCİ BİLGİLERİ GETİRİN
+
+        ResultSet veri = st.executeQuery("select * from ogrenciler where cinsiyet='E' ");
+
+        while (veri.next()){
+            System.out.printf("%-6d %-15.15s %-8s %-8s\n", veri.getInt(1),
+                    veri.getString(2), veri.getString(3),
+                    veri.getString(4));
+        }
+
+
+        //SORU02: ÖĞRENCİLER TABLOSUNDAKİ  9 SINIF KIZ ÖĞRENCİ BİLGİLERİ GETİRİN
+
+        System.out.println("==========================================================================");
+
+        ResultSet veri2 = st.executeQuery("select * from ogrenciler where sinif=9 and cinsiyet='K'");
+
+        while (veri2.next()){
+            System.out.printf("%-6d %-15.15s %-8s %-8s\n", veri2.getInt(1),
+                    veri2.getString(2), veri2.getString(3),
+                    veri2.getString(4));
+        }
+
+    }
+}
